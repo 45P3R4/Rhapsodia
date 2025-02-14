64297e73ae722909d767097f3b94d621e67c479a
ksamoilova <langleybaka@gmail.com>
2025-02-14 17:05:09 +0300

    shadder support

--- src/main.c
+++ src/main.c
@@ -3,11 +3,16 @@
 #include "noise.h"
 #include "world.h"
 
+#define SHADER_ON
+
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+
 int main()
 {
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
-	InitWindow(1280, 720, "Rhapsodia");
+	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Rhapsodia");
 
 	// SearchAndSetResourceDir("resources");
 
@@ -25,41 +30,57 @@ int main()
 
 	worldInit();
 
-	Chunk ch;
-	ch.position = (Vector3){1,1,0};
-	fillChunkSmooth(&ch, STONE);
-	Mesh meshChunk = genMeshChunk(ch);
-	Model modelChunk = LoadModelFromMesh(meshChunk);
+	#ifdef SHADER_ON
+	Shader shader = LoadShader(0, TextFormat("resources/shaders/grayscale.fs"));
+	#endif //SHADER_ON
 
-	Material materialChunk;
-
-	modelChunk.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = LoadTexture("resources/grass.png");
-
-	// Mesh meshBlock = genMeshBlock((Vector3){0,-4,0}, DIRT);
-	// Model modelBlock = LoadModelFromMesh(meshBlock);
+	RenderTexture2D target = LoadRenderTexture(SCREEN_WIDTH, SCREEN_HEIGHT);
 
 	Vector3 position = (Vector3){0,0,0};
 	// game loop
 	while (!WindowShouldClose()) // run the loop untill the user presses ESCAPE or presses the Close button on the window
 	{
+
+	#ifdef SHADER_ON
+		UpdateCamera(&camera, CAMERA_FREE);
+
+		BeginTextureMode(target);       // Enable drawing to texture
+			ClearBackground(skyColor);  // Clear texture background
+            BeginMode3D(camera);        // Begin 3d mode drawing
+			drawChunks();
+                DrawGrid(10, 1.0f);     // Draw a grid
+            EndMode3D();                // End 3d mode drawing, returns to orthographic 2d mode
+        EndTextureMode(); 
+
+		BeginDrawing();
+		ClearBackground(skyColor); 
+
+		BeginShaderMode(shader);
+			DrawTextureRec(target.texture, (Rectangle){ 0, 0, (float)target.texture.width, (float)-target.texture.height }, (Vector2){ 0, 0 }, WHITE);
+        EndShaderMode();
+
+		EndShaderMode();
+		EndMode3D();
+
+		DrawFPS(3, 3);
+		EndDrawing();
+	#else
 		UpdateCamera(&camera, CAMERA_FREE);
 
 		BeginDrawing();
-		ClearBackground(skyColor);
-		BeginMode3D(camera);
+		ClearBackground(skyColor); 
 
-		DrawGrid(10, 1);
-		// DrawModel(modelChunk, position, 1, WHITE);
-		// DrawModel(modelBlock, position, 1, BROWN);
-		drawChunks();
-		DrawCube((Vector3){2, 0, 0}, 2, 0.1, 0.1, RED);
-		DrawCube((Vector3){0, 2, 0}, 0.1, 2, 0.1, GREEN);
-		DrawCube((Vector3){0, 0, 2}, 0.1, 0.1, 2, BLUE);
-		// drawChunks();
-
+		ClearBackground(skyColor);  // Clear texture background
+            BeginMode3D(camera);        // Begin 3d mode drawing
+			drawChunks();
+                DrawGrid(10, 1.0f);     // Draw a grid
+            EndMode3D();  
 		EndMode3D();
+
 		DrawFPS(3, 3);
 		EndDrawing();
+	#endif //SHADER_ON
+		
 	}
 
 	CloseWindow();
